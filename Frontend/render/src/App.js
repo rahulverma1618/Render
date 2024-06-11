7116474505dda885e1af53991f9ae108a78be78f
rahul7060 <rs0275645@gmail.com>
2024-06-11 16:54:51 +0530

    creating navbar

--- Frontend/render/src/App.js
+++ Frontend/render/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import Navbar from './components/navbar/navbar';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
